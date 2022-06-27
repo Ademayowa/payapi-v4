1a2b098065f07619ba2ebbf74d634f68eb313731
Ademayowa <ademayowaadeniyi@gmail.com>
2022-06-27 09:51:25 +0100

    changed footer link to homepage

--- src/components/Banking/Banking.js
+++ src/components/Banking/Banking.js
@@ -1,30 +1,9 @@
-import React, { useState } from 'react';
-import Img1 from '../../img/finances.svg';
-import Img2 from '../../img/banking.svg';
-import Img3 from '../../img/payments.svg';
+import React from 'react';
 import { data } from '../../data';
 import BankingList from '../BankingList/BankingList';
 import './Banking.css';
 
 const Banking = () => {
-  // const [banking] = useState([
-  //   {
-  //     img: Img1,
-  //     title: 'Personal Finances',
-  //     info: 'Consolidate financial data from multiple sources and categorize transactions up to 2 years of history. Analyze reports to reconcile activities in your account.',
-  //   },
-  //   {
-  //     img: Img2,
-  //     title: 'Banking & Coverage',
-  //     info: 'With our platform, you can speed up account onboarding and support ongoing payments for checking, savings, credit card, and brokerage accounts.',
-  //   },
-  //   {
-  //     img: Img3,
-  //     title: 'Consumer Payments',
-  //     info: 'It’s easier to set up secure bank payments with us through a flow designed with the user experience in mind. Customers could instantly authenticate their account.',
-  //   },
-  // ]);
-
   return (
     <section className='banking container'>
       <div className='row' data-aos='fade-up'>
